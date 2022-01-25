d2cdad221dc2a62ff297ff1c10726278383859e4
Harsh <rahulgahlawat32@gmail.com>
2022-01-25 10:17:27 +0530

    Changed Directory

---
+++ Greedy/activity_selection.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void algo(int arr[], int size) {
+    
+}
+
+int main() {
+    int arr[] = {};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    algo(arr, size);
+    return 0;
+}
